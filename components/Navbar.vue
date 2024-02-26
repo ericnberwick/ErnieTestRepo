315e264c114eb299299c993033596d971e227e74
ericnberwick <nb15galle@gmail.com>
2024-02-26 17:55:46 +0000

    About us page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,41 +1,41 @@
 <template>
-    <nav class="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+    <nav class="p-5 bg-white shadow 2xl:flex 2xl:items-center 2xl:justify-between">
         <div class="flex justify-between items-center ">
             <span class="font-bold text-2xl font-[Arial] cursor-pointer hover:text-orange-500 duration-500 truncate">
                 <img class="h-10 inline px-4" src="/img/predict.jpg">
                 <NuxtLink to="/">E.P. Chan & Associates</NuxtLink>
             </span>
 
-            <span class="text-3xl cursor-pointer md:hidden block mx-2 mb-2">
+            <span class="text-3xl cursor-pointer 2xl:hidden block mx-2 mb-2">
                 
                 <Icon class="hover:text-orange-500 duration-500" name="mingcute:align-justify-fill"></Icon>
             </span>
         </div>
 
-        <ul class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7
-                    md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
-            <li class="mx-4 my-6 md:my-0">
+        <ul class="2xl:flex 2xl:items-center z-[-1] 2xl:z-auto 2xl:static absolute bg-white w-full left-0 2xl:w-auto 2xl:py-0 py-4 2xl:pl-0 pl-7
+                    2xl:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
+            <li class="mx-4 my-6 2xl:my-0">
                 <NuxtLink to="/" class="text-xl font-bold hover:text-orange-500 duration-500 cursor-pointer">Home</NuxtLink>
             </li>
-            <li class="mx-4 my-6 md:my-0 truncate">
+            <li class="mx-4 my-6 2xl:my-0 truncate">
                 <NuxtLink to="/about" class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Financial
                     Machine Learning</NuxtLink>
             </li>
-            <li class="mx-4 my-6 md:my-0">
+            <li class="mx-4 my-6 2xl:my-0">
                 <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Workshops</NuxtLink>
             </li>
-            <li class="mx-4 my-6 md:my-0">
+            <li class="mx-4 my-6 2xl:my-0">
                 <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Books</NuxtLink>
             </li>
-            <li class="mx-4 my-6 md:my-0 truncate">
+            <li class="mx-4 my-6 2xl:my-0 truncate">
                 <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Investment Management
                 </NuxtLink>
             </li>
-            <li class="mx-4 my-6 md:my-0">
-                <NuxtLink to="/workshops-store" class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">
+            <li class="mx-4 my-6 2xl:my-0">
+                <NuxtLink to="/about" class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">
                     About Us</NuxtLink>
             </li>
-            <li class="mx-4 my-6 md:my-0">
+            <li class="mx-4 my-6 2xl:my-0">
             <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Email</NuxtLink>
         </li>
     </ul>
--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,61 @@
 <template>
-    
+    <!-- border-sky-400 border-2  -->
     <div>
+        <Navbar></Navbar>
 
-        <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             Nostrum ducimus praesentium eum nemo sint, blanditiis distinctio,
-              at sequi voluptate alias temporibus adipisci provident rem dolore quos 
-              sapiente vel mollitia. Animi?</p>
+        <div class="mx-8 my-4">
+            <header class="flex justify-center ">
+                <h2>About Us</h2>
+            </header>
 
+            <div class="flex justify-center">
+                <div class=" w-1/2">
+                    <hr>
+                    <img class ="mr-4 float-left" width="201" src="/img/epchanDotCom.bak.png">
+                    <p class="text-lg"> 
+                        Dr. Ernest P. Chan,  <b>founder</b>, <a class="underline" href="https://predictnow.ai/">PredictNow.ai Inc</a>. Ernie’s career since 1994 has been focusing on the development
+                        of statistical models and advanced computer algorithms to find patterns and trends in large quantities of data.
+                        He has applied his expertise in machine learning at IBM T.J. Watson Research Center’s Human Language Technologies group,
+                        at Morgan Stanley’s Data Mining and Artificial Intelligence Group, and at Credit Suisse’s Horizon Trading Group.
+                        He is also the founder and managing member of a quantitative investment management firm,
+                        <a class="underline" href="https://web.archive.org/web/20220403205741/https://qtscm.com/">QTS Capital Management, LLC.</a></p>
+                    
+                    <p class="text-lg"> Ernie was quoted by the <a class="underline" href="https://www.wsj.com/articles/volatility-is-everywhere-the-market-tactic-thats-driving-stocks-haywire-11591977978?mod=searchresults&page=1&pos=1">Wall Street Journal</a>
+                        , <a class="underline" href="https://web.archive.org/web/20220327065842/http://www.nytimes.com/2007/08/13/business/13hedge.html?adxnnl=1&ref=business&adxnnlx=1305116266-X4+E/4saVgOyqb+n13H7QQ">New York Times</a>
+                        , <a class="underline" href="https://www.forbes.com/sites/jeremybogaisky/2013/09/04/quants-r-us-algorithmic-trading-trickles-down-to-individual-investors/">Forbes</a>, and the 
+                        <a class="underline" href="https://blogs.cio.com/elana_varon/lessons_for_innovators_in_quant_fund_sell_off">CIO magazine</a>, and interviewed on CNBC’s Closing
+                        Bell program, <a class="underline" href="https://www.traders.com/Documentation/FEEDbk_Docs/2009/07/Interview.html">Technical Analysis of Stocks and Commodities</a> magazine, Securities Industry News, 
+                        <a class="underline" href="http://www.automatedtrader.net/articles/first-person/149144/the-accidental-quant">Automated Trader</a> magazine, and
+                        the <a class="underline" href="http://www.cfapubs.org/doi/pdf/10.2469/cfm.v25.n1.9">CFA Institute Magazine</a> on topics related to quantitative trading. In recognition of his expertise in statistical data mining, 
+                        he was invited to serve on the Program Committees of the International Conference of Knowledge Discovery and Data Mining in 1998. 
+                        He is the author of <a class="underline" href="https://www.amazon.com/dp/0470284889/ref=as_sl_pd_tf_lc?tag=quantitativet-20&camp=14573&creative=327641&linkCode=as1&creativeASIN=0470284889&adid=0VQTVRMGV5EDCHH9V8F4&&ref-refURL=http%3A%2F%2Fepchan.blogspot.ca%2F">“Quantitative Trading: How to Build Your Own Algorithmic Trading Business“</a>, 
+                        <a class="underline" href="https://www.amazon.com/gp/product/1118460146/ref=as_li_qf_sp_asin_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1118460146&linkCode=as2&tag=quantitativet-20">“Algorithmic Trading: Winning Strategies 
+                        and Their Rationale“</a>, and <a class="underline" href="https://www.amazon.com/gp/product/1119219604/ref=as_li_tl?ie=UTF8&tag=quantitativet-20&camp=1789&creative=9325&linkCode=as2&creativeASIN=1119219604&linkId=97db6ca4a561e27f8410b6848e916a27">“Machine Trading: Deploying Computer Algorithms to Conquer the Markets“</a>
+                        , all published by John Wiley & Sons. 
+                        He also writes the popular <a class="underline" href="https://epchan.blogspot.com/">Quantitative Trading blog</a> and conducts workshops on quantitative investment strategies and machine learning in London, UK. 
+                        He was an Adjunct Associate Professor of Finance at Nanyang Technological University in Singapore, and an Industry Fellow of the NTU-SGX Centre for 
+                        Financial Education, which is jointly set up by NTU and the Singapore Exchange. He is an adjunct faculty at Northwestern University’s Master’s in 
+                        Data Science program and supervises student theses there.</p>
+                    
+                    <p class="text-lg"> Ernie holds a Bachelor of Science degree from University of Toronto in 1988, a Master of Science (1991) and a Doctor of Philosophy (1994) degree in theoretical
+                        physics from Cornell University.</p>
+                    <hr>
+                    <img class ="mr-4 float-left w-50 " width="201" src="/img/Roger-photo.bak.jpg">
+                    <p class="text-lg"> Dr. Roger Hunter, scientific adviser,  <a class="underline" href="https://predictnow.ai/">PredictNow.ai Inc</a>. Roger is an expert in designing high-performance automated execution system as well as machine learning
+                         software. Roger was the founder and former manager of a highly profitable equities fund, and was the founder and former CEO of a scientific software firm that 
+                         was acquired by Thomson Reuters. His firm’s software is currently in use at the U.S. Federal Reserve. Roger was formerly professor of mathematics a
+                         t New Mexico State University, and he obtained his Ph.D. in Mathematics from Australian National University. Roger was profiled in a <a class="underline" href="https://www.bloomberg.com/news/articles/2016-03-16/barbarian-coders-at-the-gate-the-inexorable-rise-of-diy-quants">Bloomberg Businessweek article.</a></p>
+                    <div class="mt-20">
+                        <hr class="border-black border-2">
+                    
+                            <p><Icon name="ic:round-copyright"> </Icon> 2022 . E.P. Chan & Associates</p>
+                        
+                    </div>    
+                </div>
+                
+            </div>
+            
+        </div>
     </div>
 </template>
 
