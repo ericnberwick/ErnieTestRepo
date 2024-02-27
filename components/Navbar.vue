9c8a41e94470efc2a791544ea5965ed1a0fee334
ericnberwick <nb15galle@gmail.com>
2024-02-27 09:57:57 +0000

    Navbar complete

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,45 +1,59 @@
 <template>
-    <nav class="p-5 bg-white shadow 2xl:flex 2xl:items-center 2xl:justify-between">
-        <div class="flex justify-between items-center ">
-            <span class="font-bold text-2xl font-[Arial] cursor-pointer hover:text-orange-500 duration-500 truncate">
-                <img class="h-10 inline px-4" src="/img/predict.jpg">
-                <NuxtLink to="/">E.P. Chan & Associates</NuxtLink>
-            </span>
+    
+    <div class="bg-white w-full flex justify-center">
+        
+        <div class="w-1/2 border-black border-2 rounded-md mt-4">
+            <div class="w-full flex justify-between bg-white">
+                <h2 class="text-black pl-4 my-2 font-bold text-4xl">E.P. Chan & Associates</h2>
+                <h3 class="text-slate-400 pr-4 my-2 text-2xl truncate">Quantitative Trading</h3>
+            </div>
+            <div class="w-auto">
+                <img src="/img/header2.jpg">
+            </div>
+            <nav class="p-5 bg-white shadow 2xl:flex 2xl:items-center 2xl:justify-center w-auto">
+                <div class="flex justify-between items-center ">
+                    <!-- <span class="font-bold text-2xl font-[Arial] cursor-pointer hover:text-orange-500 duration-500 truncate">
+                        
+                        <NuxtLink to="/">E.P. Chan & Associates</NuxtLink>
+                    </span> -->
 
-            <span class="text-3xl cursor-pointer 2xl:hidden block mx-2 mb-2">
-                
-                <Icon class="hover:text-orange-500 duration-500" name="mingcute:align-justify-fill"></Icon>
-            </span>
+                    <span class="text-3xl cursor-pointer 2xl:hidden block mx-2 mb-2">
+                        
+                        <Icon class="hover:text-orange-500 duration-500" name="mingcute:align-justify-fill"></Icon>
+                    </span>
+                </div>
+
+                    <ul class="2xl:flex 2xl:items-center z-[-1] 2xl:z-auto 2xl:static absolute bg-white w-full left-0 2xl:w-auto 2xl:py-0 py-4 2xl:pl-0 pl-7
+                                2xl:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
+                        <li class="mx-1 my-6 2xl:my-0">
+                            <NuxtLink to="/" class="text-sm font-bold hover:text-orange-500 duration-500 cursor-pointer">Home</NuxtLink>
+                        </li>
+                        <li class="mx-4 my-6 2xl:my-0 truncate">
+                            <NuxtLink to="/about" class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Financial
+                                Machine Learning</NuxtLink>
+                        </li>
+                        <li class="mx-4 my-6 2xl:my-0">
+                            <NuxtLink class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Workshops</NuxtLink>
+                        </li>
+                        <li class="mx-4 my-6 2xl:my-0">
+                            <NuxtLink class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Books</NuxtLink>
+                        </li>
+                        <li class="mx-4 my-6 2xl:my-0 truncate">
+                            <NuxtLink class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Investment Management
+                            </NuxtLink>
+                        </li>
+                        <li class="mx-4 my-6 2xl:my-0">
+                            <NuxtLink to="/about" class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">
+                                About Us</NuxtLink>
+                        </li>
+                        <li class="mx-1 my-6 2xl:my-0">
+                        <NuxtLink class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Email</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
         </div>
-
-        <ul class="2xl:flex 2xl:items-center z-[-1] 2xl:z-auto 2xl:static absolute bg-white w-full left-0 2xl:w-auto 2xl:py-0 py-4 2xl:pl-0 pl-7
-                    2xl:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
-            <li class="mx-4 my-6 2xl:my-0">
-                <NuxtLink to="/" class="text-xl font-bold hover:text-orange-500 duration-500 cursor-pointer">Home</NuxtLink>
-            </li>
-            <li class="mx-4 my-6 2xl:my-0 truncate">
-                <NuxtLink to="/about" class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Financial
-                    Machine Learning</NuxtLink>
-            </li>
-            <li class="mx-4 my-6 2xl:my-0">
-                <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Workshops</NuxtLink>
-            </li>
-            <li class="mx-4 my-6 2xl:my-0">
-                <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Books</NuxtLink>
-            </li>
-            <li class="mx-4 my-6 2xl:my-0 truncate">
-                <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Investment Management
-                </NuxtLink>
-            </li>
-            <li class="mx-4 my-6 2xl:my-0">
-                <NuxtLink to="/about" class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">
-                    About Us</NuxtLink>
-            </li>
-            <li class="mx-4 my-6 2xl:my-0">
-            <NuxtLink class="text-xl hover:text-orange-500 duration-500 cursor-pointer font-bold">Email</NuxtLink>
-        </li>
-    </ul>
-</nav></template>
+    </div>
+</template>
 
 <script setup>
 
