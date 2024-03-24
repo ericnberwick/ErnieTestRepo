a306d99df2a3702ef336a761ce36cb8c8e4b3a57
ericnberwick <nb15galle@gmail.com>
2024-03-24 11:31:02 +0000

    Workshop page done

--- pages/financial-machine-learning.vue
+++ pages/financial-machine-learning.vue
@@ -19,80 +19,80 @@
                         <b>a financial machine learning SaaS</b> that computes the <b>Probability of Profit</b> for your next investment.
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         We highlight some of the difficulties of financial ML below. and how <a href="https://predictnow.ai/" class="underline">PredictNow.ai</a> can help your investment management practice overcome them. 
                     </p>
 
                     <h2>Financial data scrubbing </h2>
 
-                    <p>
+                    <p class="text-lg">
                         This may seem like a task for plumbers, but is actually the crucial first step in successful financial machine learning, and it takes surprisingly 
                         deep domain knowledge to accomplish. For example, do you know that many fundamental stocks databases have look-ahead bias? How would you find out 
                         if your news sentiment data is sensible? With years of financial and data science experience behind us, we can help you find out.  
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         See Part 1 of Dr. Chan and Dr. Hunter’s <a href="https://epchan.com/workshops/" class="underline">Lifecycle of Trading Strategy Development course</a> to learn about detecting and fixing myriad problems of financial data.
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         <a href="https://predictnow.ai/" class="underline">PredictNow.ai</a> has a team of financial data scientists that can do that for you.
                     </p>
 
                     <h2>Features engineering and selection</h2>
-                    <p>
+                    <p class="text-lg">
                         We can help identify, combine, select and rank variables that affect the returns of an investment strategy.
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         See Dr. Chan’s <a href="https://epchan.com/wp-content/uploads/2022/04/Machine-Learning-in-Finance.pdf" class="underline">Risk.net talk</a>, Toronto, September 2018.
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         <a href="https://predictnow.ai/" class="underline">PredictNow.ai</a> has a team of financial data scientists that can do that for you
                     </p>
 
                     <h2>Predicting “non-reflexive”* targets</h2>
 
-                    <p>
+                    <p class="text-lg">
                         If returns can be predicted, returns will change in response to the prediction. On the other hand,
                          if weather can be predicted, weather will not change in response. Yet accurate weather prediction
                           can benefit agricultural futures traders.
                     </p>
 
-                    <p>
+                    <p class="text-lg"> 
                         <a href="https://predictnow.ai/" class="underline">PredictNow.ai</a> can help identify and predict non-reflexive targets (e.g. earnings, same-store sales, etc.) that cannot be arbitraged away.
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         * Reflexivity is a term used by George Soros to describe the effects of arbitrage activities on the financial markets and the general economy. 
                     </p>
 
                     <h2>Capital allocation and risk management via “meta-labelling”*</h2>
-                    <p>
+                    <p class="text-lg">
                         Determine probability of profit for a trade through machine learning models, and allocate capital and manage risks accordingly.
                          These are part of “<b>quantamental strategies</b>“: applying machine learning to help discretionary or fundamental investment managers
                           quantify and systematize their ideas, factors, and knowledge, done without the necessity of disclosing the fundamental strategy
                            to the consultant.
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         See Dr. Chan’s <a class="underline" href="https://epchan.com/wp-content/uploads/2022/04/Meta-labelling.pdf">Quant World Canada talk</a>, Toronto, November, 2018.
                     </p>
                     
-                    <p>
+                    <p class="text-lg">
                         Predicting the<b> Probability of Profit</b> is exactly what <a class="underline" href="https://web.archive.org/web/20200812223937/https://predictnow.ai/">PredictNow.ai</a> SaaS does!
                     </p>
 
-                    <p>
+                    <p class="text-lg">
                         * Meta-labelling is a term coined by Dr. López de Prado in his book “Advances in Financial Machine Learning”.
                     </p>
 
                     <h2>Market simulations for performance evaluation</h2>
-                    <p>
+                    <p class="text-lg">
                         Using neural networks to capture essential market patterns, and generate realistic simulations for trading strategies evaluation and risk assessment.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         See Dr. Chan’s <a  class="underline" href="https://epchan.com/wp-content/uploads/2022/04/Optimizing-Trading-Strategies-Without-Overfitting.pdf">QuantCon keynote speech</a>, New York, 2018.
                     </p>
 
@@ -117,61 +117,61 @@
                         Our Research Articles and Presentations 
                     </h2>
 
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“What is the probability of profit of your next trade? (Introducing PredictNow.ai)”</a> explains in details how you can use our SaaS to improve your existing investment or trading strategy.
                     </p>
                     <p>
                         <a class="underline" href="">“The best way to select features?“</a>, preprint by Xin Man and Ernest Chan comparing MDA, LIME, and SHAP feature selection methods in machine learning.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“US nonfarm employment prediction using RIWI Corp. alternative data“</a>, working paper by Radu Ciobanu and Ernest Chan on using alternative data to predict NFP numbers.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Financial Data Science and Machine Learning”</a> webinar at futures.io, October 2019.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“What we learned from Kaggle Two-Sigma News Sentiment competition?“</a>, UNICOM conference keynote speech, London, June 2019.
                     </p>
-                    <p>
+                    <p class="text-lg"> 
                         <a class="underline" href="">“Is News Sentiment Still Adding Alpha?“</a>, blog post on epchan.blogspot.com on our News Sentiment research.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Why features importance should only be computed in validation set“</a>, an open source kernel in Kaggle Two-Sigma competition.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“What to do before machine learning?“</a>, Quantopian webinar, October 2018. (Video.)
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“FX Order Flow as a Predictor”</a> at Friedberg Mercantile Group event in Toronto, May 2018.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Optimizing Trading Strategies Without Overfitting”</a>, keynote speech at QuantCon, New York, April 2018.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Applying machine learning techniques to everyday strategies”</a> at Grupo L&S conference, Brazil, November 2017.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Enhancing Statistical Significance of Backtests”</a> at QuantCon, New York, April 2017.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“The Peculiarity of Volatility”</a> at QuantCon, New York, April 2016.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Optimal Order Types”</a> at Market Microstructure, Liquidity and Automated Trading conference, London, July 2015.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Beware of Low Frequency Data”</a> at QuantCon, New York, March 2015.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Factor Models in Practice”</a> at Society of Technical Analysts, London, Oct 2014.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“Backtesting and Its Pitfalls”</a>. Market Technician Association meeting, London, Apr 2013.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="">“How to Succeed in the Quant Trading Business”</a>. York University Schulich School of Business Professional Seminar Series. January, 2013.
                     </p>
-                    <p>
+                    <p class="text-lg">
                         <a class="underline" href="https://epchan.com/wp-content/uploads/2022/04/Discovering-Risk-Indicators-in-the-FX-Markets.pdf">“Discovering Risk Indicators in the FX Markets”</a> at Quant Invest Canada conference in Toronto, Canada, October 2012.
                     </p>
                     
--- pages/workshops.vue
+++ pages/workshops.vue
@@ -14,10 +14,124 @@
                     </header>
                     <hr>
                     
-                    <p class="text-lg"> 
-                        Dr. Chan is the founder of QTS Capital Management, LLC., a commodity pool operator and commodity trading advisor.
-                         QTS manages a commodity pool (hedge fund) as well as individual brokerage accounts. Please visit www.qtscm.com for an overview of its service.
+                    <ul class="text-lg  list-disc list-inside"> 
+                        <li class="text-lg">How can you tell if your earnings data has look-ahead bias?</li>
+                        <li class="text-lg">How would you know if your vendor’s news sentiment data has errors?</li>
+                        <li class="text-lg">What are the many hidden problems with simple stock price databases?.</li>
+                    </ul>
+
+                    <p class="text-lg">
+                            These and many other topics will be discussed in our new pre-recorded online course
+                            <b>Lifecycle of Trading Strategy Development with Machine Learning</b>. This is a 12-hour,
+                            in-depth, online workshop focusing on the challenges and nuances of working with
+                            financial data and applying machine learning to generate trading strategies.
+                            We will walk you through the complete lifecycle of trading strategies creation 
+                            and improvement using machine learning, including automated execution, with unique 
+                            insights and commentaries from our own research and practice. We will make extensive 
+                            use of <b>Python</b> packages such as Pandas, Scikit-learn, LightGBM, and execution platforms
+                            like QuantConnect. It will be co-taught by Dr. Ernest Chan and Dr. Roger Hunter,
+                            principals of QTS Capital Management, LLC. The course was taught in 3 parts, each
+                            part will last 4 hours. <b>Course</b> outline is available for <u>download</u>. Inquiries at <u><a href="mailto:ernest@epchan.com">ernest@epchan.com</a></u>
                     </p>
+
+                    <ul class="text-lg  list-disc list-inside"> 
+                        <li class="text-lg">Fee: $599 per part. ($1,749 for complete course. Full-time student discount available.)</li>
+                        <li class="text-lg">Dates and times: Pre-recorded</li>
+                        <li class="text-lg">Where: Online</li>
+                    </ul>
+
+                    <div>
+                        <p>
+                            Part 1. <i>Financial data science and features engineering</i> 
+                        </p>
+                        <button class=" rounded-xl bg-yellow-400 p-2 hover:bg-yellow-500  transition-all">
+                            <b>Buy Now</b>
+                        </button>
+
+                    </div>
+
+                    <div>
+                        <p>
+                            Part 2. <i>Financial machine learning </i>
+                        </p>
+                        <button class=" rounded-xl bg-yellow-400 p-2 hover:bg-yellow-500  transition-all">
+                            <b>Buy Now</b>
+                        </button>
+
+                    </div>
+
+                    <div>
+                        <p>
+                            Part 3. <i>Backtesting ML strategies and automated execution</i>
+                        </p>
+                        <button class=" rounded-xl bg-yellow-400 p-2 hover:bg-yellow-500  transition-all">
+                            <b>Buy Now</b>
+                        </button>
+
+                    </div>
+
+                    <div class="my-4">
+                        <p>
+                            <b>Complete course</b> Parts 1 – 3
+                        </p>
+                        <button class=" rounded-xl bg-yellow-400 p-2 hover:bg-yellow-500  transition-all">
+                            <b>Buy Now</b>
+                        </button>
+
+                    </div>
+
+                    <hr>
+                    <p class="text-lg">
+                        The <b>complete list of our pre-recorded online courses</b> can be downloaded <u>here</u>. Please email ernest@epchan.com to register.
+                    </p>
+                    <hr>
+
+                    <p>
+                        Praise for our workshops
+                    </p>
+
+                    <p>
+                        <i>“An excellent course by a great teacher. Ernie clearly explained and applied the different areas of Artificial Intelligence
+                        , provided invaluable insights as to their relative merits, and gave me the confidence to implement them in my own trading.”</i>
+                         – Dr Nikhil Shenai (Ph.D., Imperial College, BA, University of Cambridge), Founder of E K Technologies
+                        (Quantitative Trading & Development)
+                    </p>
+
+                    <p>
+                        <i>“While being familiar with the subject I truly enjoyed the material covered and the [options] course has certainly provided food
+                         for thought.”</i> – Laurent Hoffmann, Ph.D., Former Senior Quantitative Analyst, Structured Credit Trading, at Depfa Bank Asset Management Europe & RoW.
+                    </p>
+
+                    <p>
+                        <i>“…thank you again for the Momentum Strategies training course this week. It was very beneficial. I found your explanations of the concepts very clear 
+                        and the examples well developed. I like the rigorous approach that you take to strategy evaluation.”</i> – Andrew B. (senior executive at a major Canadian bank.)
+                    </p>
+
+                    <p>
+                        <i>“…I have been very impressed with Ernie’s past workshops…”</i> – Stephen Hope, Former Head of Fixed Income Quantitative Trading Strategies, BNP Paribas.
+                    </p>
+
+                    <p>
+                        <i>“Ernie’s workshop offers particularly helpful insights in implementing profitable trading strategies and that’s beyond his books’ content. And he is
+                         one of the most patient and giving instructors I ever met“</i> – K.W. Fung, CQF, Founder of Quants Investment 
+                    </p>
+
+                    <p>
+                        <i>“These workshops have provided me with enough familiarity and confidence to tackle the latest research. Just the segment on intermarket sweep orders in
+                         the MFT course was worth the price of admission to all three workshops I went to.“</i> – Cedric Yau 
+                    </p>
+
+                    <p>
+                        <i>“Dr. Chan brings a practical approach to a difficult topic that helps students not only understand the topic at hand but why the topic is important in 
+                        the applied field. I can’t speak highly enough about the course and instruction…”</i> – Anonymous student evaluation
+                    </p>
+
+                    <p>
+                        <i>“Dr. Chan … is a phenomenal instructor…”</i> – Anonymous student evaluation  
+                    </p>
+
+
+                    
                     
                     
 
