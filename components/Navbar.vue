546469abbfd6b00b33dcad600000bc2bc4191ab1
ericnberwick <nb15galle@gmail.com>
2024-03-25 08:29:59 +0000

    Responsive navbar done

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-    
+
     <div class="bg-white w-full flex justify-center">
-        
+
         <div class="w-1/2 border-black border-2 rounded-md mt-4">
             <div class="w-full flex justify-between bg-white">
                 <h2 class="text-black pl-4 my-2 font-bold text-4xl">E.P. Chan & Associates</h2>
@@ -17,45 +17,109 @@
                         <NuxtLink to="/">E.P. Chan & Associates</NuxtLink>
                     </span> -->
 
-                    <span class="text-3xl cursor-pointer 2xl:hidden block mx-2 mb-2">
-                        
-                        <Icon class="hover:text-orange-500 duration-500" name="mingcute:align-justify-fill"></Icon>
+                    <span class="text-3xl cursor-pointer 2xl:hidden block mx-2 mb-2 ">
+
+                        <Icon class="hover:text-orange-500 duration-500 " name="mingcute:align-justify-fill" @click="toggleNavbarUI" v-show="!showNavbarUI"></Icon>
+                        <Icon class="hover:text-orange-500 duration-500 " name="maki:cross" @click="toggleNavbarUI" v-show="showNavbarUI"></Icon>
+                        <div class="bg-orange-500 text-white w-full"  name="mingcute:align-justify-fill"
+                            v-if="showNavbarUI">
+                            <ul>
+                                <li class="px-2 truncate hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to="/"
+                                        class="text-sm font-bold ">Home
+                                    </NuxtLink>
+                                </li>
+                                <li class="px-2 truncate hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to="/financial-machine-learning"
+                                        class="text-sm  duration-500 cursor-pointer font-bold">
+                                        Financial
+                                        Machine Learning</NuxtLink>
+                                </li>
+                                <li class="px-2 hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to='/workshops'
+                                        class="text-sm  duration-500 cursor-pointer font-bold">
+                                        Workshops</NuxtLink>
+                                </li>
+                                <li class="px-2 hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to='/books'
+                                        class="text-sm  duration-500 cursor-pointer font-bold">
+                                        Books</NuxtLink>
+                                </li>
+                                <li class="px-2 truncate hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to="/investment-management"
+                                        class="text-sm  duration-500 cursor-pointer font-bold">
+                                        Investment Management
+                                    </NuxtLink>
+                                </li>
+                                <li class="px-2 hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to="/about"
+                                        class="text-sm  duration-500 cursor-pointer font-bold">
+                                        About Us</NuxtLink>
+                                </li>
+                                <li class="px-2 hover:bg-orange-400 duration-500 cursor-pointer">
+                                    <NuxtLink to="/email"
+                                        class="text-sm  duration-500 cursor-pointer font-bold">
+                                        Email</NuxtLink>
+                                </li>
+                            </ul>
+                        </div>
                     </span>
+
                 </div>
 
-                    <ul class="2xl:flex 2xl:items-center z-[-1] 2xl:z-auto 2xl:static absolute bg-white w-full left-0 2xl:w-auto 2xl:py-0 py-4 2xl:pl-0 pl-7
+                <ul class="2xl:flex 2xl:items-center z-[-1] 2xl:z-auto 2xl:static absolute bg-white w-full left-0 2xl:w-auto 2xl:py-0 py-4 2xl:pl-0 pl-7
                                 2xl:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 ">
-                        <li class="mx-1 my-6 2xl:my-0">
-                            <NuxtLink to="/" class="text-sm font-bold hover:text-orange-500 duration-500 cursor-pointer">Home</NuxtLink>
-                        </li>
-                        <li class="mx-4 my-6 2xl:my-0 truncate">
-                            <NuxtLink to="/financial-machine-learning" class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Financial
-                                Machine Learning</NuxtLink>
-                        </li>
-                        <li class="mx-4 my-6 2xl:my-0">
-                            <NuxtLink to='/workshops' class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Workshops</NuxtLink>
-                        </li>
-                        <li class="mx-4 my-6 2xl:my-0">
-                            <NuxtLink to='/books' class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Books</NuxtLink>
-                        </li>
-                        <li class="mx-4 my-6 2xl:my-0 truncate">
-                            <NuxtLink to="/investment-management" class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Investment Management
-                            </NuxtLink>
-                        </li>
-                        <li class="mx-4 my-6 2xl:my-0">
-                            <NuxtLink to="/about" class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">
-                                About Us</NuxtLink>
-                        </li>
-                        <li class="mx-1 my-6 2xl:my-0">
-                        <NuxtLink to="/email" class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Email</NuxtLink>
+                    <li class="mx-1 my-6 2xl:my-0">
+                        <NuxtLink to="/" class="text-sm font-bold hover:text-orange-500 duration-500 cursor-pointer">
+                            Home</NuxtLink>
+                    </li>
+                    <li class="mx-4 my-6 2xl:my-0 truncate">
+                        <NuxtLink to="/financial-machine-learning"
+                            class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Financial
+                            Machine Learning</NuxtLink>
+                    </li>
+                    <li class="mx-4 my-6 2xl:my-0">
+                        <NuxtLink to='/workshops'
+                            class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Workshops
+                        </NuxtLink>
+                    </li>
+                    <li class="mx-4 my-6 2xl:my-0">
+                        <NuxtLink to='/books'
+                            class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Books</NuxtLink>
+                    </li>
+                    <li class="mx-4 my-6 2xl:my-0 truncate">
+                        <NuxtLink to="/investment-management"
+                            class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Investment
+                            Management
+                        </NuxtLink>
+                    </li>
+                    <li class="mx-4 my-6 2xl:my-0">
+                        <NuxtLink to="/about"
+                            class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">
+                            About Us</NuxtLink>
+                    </li>
+                    <li class="mx-1 my-6 2xl:my-0">
+                        <NuxtLink to="/email"
+                            class="text-sm hover:text-orange-500 duration-500 cursor-pointer font-bold">Email</NuxtLink>
                     </li>
                 </ul>
+
             </nav>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+var showNavbarUI = ref(false);
+
+function toggleNavbarUI() {
+  showNavbarUI.value = !showNavbarUI.value;
+  console.log("show nav bar ", showNavbarUI)
+  
+}
+
 
 </script>
 
